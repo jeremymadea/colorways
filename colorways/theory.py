c70f57866ad664f1e44e1c02445a574a6c3dfc2f
Jeremy Madea <jdmadea@gmail.com>
2022-09-24 08:58:07 -0400

    adds theory functions

--- colorways/theory.py
+++ colorways/theory.py
@@ -1,8 +1,11 @@
 from .internals import colpal_params_function, colpal_function
+from .conv import rgb2hsl, hsl2rgb
 
 __all__ = [
     'complement',
+    'hxx_complement',
     'hxx_triad',
+    'hxx_analogous',
     'hxx_square',
     'hxx_complementary_pair',
     'split_palette',
@@ -14,7 +17,18 @@ def complement(base):
     """Returns the RGB complement of the color."""
     return [1.0-a for a in base]
 
+@colpal_function
+def hxx_complement(base):
+    """Returns the Hxx complement of the color."""
+    return [(base[0]+.5)%1, base[1], base[2]]
 
+@colpal_function
+def hxx_analogous(base):
+    return [ 
+        [(base[0] - (1/6.0))%1.0, base[1], base[2]],
+        list(base),
+        [(base[0] + (1/6.0))%1.0, base[1], base[2]]
+    ]
 
 @colpal_function
 def hxx_triad(base):
@@ -57,11 +71,18 @@ def split_palette(base, angles):
 
 
 @colpal_params_function
-def tetradic_palette(base, angles):
+def tetradic_palette(base, angle):
     """
-    Undocumented.
+    Takes a base color and an angle in [0, 1] turns. Base is assumed to be in
+    Hxx space. A secondary base is specified by the angle from the base. The 
+    returned palette consists of the base, its complement, the secondary base,
+    and its complement. This scheme is otherwise known as a rectangular color 
+    scheme.
     """
-    return [[(base[0]-a+.5)%1.0, base[1], base[2]] for a in angles 
-           ] + [list(base)] + [ 
-            [(base[0]+a)%1.0, base[1], base[2]] for a in angles]
+    return [
+            list(base), 
+            #rgb2hsl(complement(hsl2rgb(base))),  
+            hxx_complement(base),  
+            [(base[0]+angle)%1, base[1], base[2]], 
+            [(base[0]+angle+.5)%1, base[1], base[2]] ]
 
