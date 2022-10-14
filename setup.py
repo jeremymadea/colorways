6d93ff669640cc9762bd8785a335f39c142450cc
Jeremy Madea <jdmadea@gmail.com>
2022-10-14 13:24:18 -0400

    starts prep for pypi release.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='colorways',
+      version='0.9.0',
+      description='The funniest joke in the world',
+      url='https://github.com/jeremymadea/colorways',
+      author='Jeremy Madea',
+      author_email='jdmadea@gmail.com',
+      license='MIT',
+      packages=['colorways'],
+      zip_safe=False)
